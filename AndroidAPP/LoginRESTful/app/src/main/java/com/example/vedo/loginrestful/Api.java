289ef3a8c273cadc8fbf67fa0d9a9edeb5bac522
Vedovati <vedovati.14170@itispaleocapa.it>
2019-05-13 19:27:34 +0200

    add android finihed app

--- AndroidAPP/LoginRESTful/app/src/main/java/com/example/vedo/loginrestful/Api.java
+++ AndroidAPP/LoginRESTful/app/src/main/java/com/example/vedo/loginrestful/Api.java
@@ -2,8 +2,7 @@ package com.example.vedo.loginrestful;
 
 public class Api {
 
-    // Path to the Api when i tested the APP
-    private static final String ROOT_URL = "http://192.168.1.108/gep/ImageBook/api/v1/Api.php?apicall=";
+    private static final String ROOT_URL = "http://vedovati.altervista.org/api/v1/Api.php?apicall=";
 
     public static final String URL_SIGNUP = ROOT_URL + "signup";
     public static final String URL_LOGIN = ROOT_URL + "login";
